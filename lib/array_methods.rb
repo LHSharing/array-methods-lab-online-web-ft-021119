b7f5d5eea9c75e717906aa2a88bd2965ba53a87c
Lu Daley <daleyluis@gmail.com>
2019-02-15 15:31:51 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,16 @@
+require 'pry'
 def using_include(array, element)
-
+#takes in arguments of an array and an element and returns true if the array includes that element, false if it does not.
+  #Array with 6 elements = ["wow", "I", "am", "really", "learning", "arrays!"]
+  #"wow"
+#binding.pry
+array.include?(element) ? true:false
 end
 
 def using_sort(array)
+#takes in an argument of an array and returns the sorted array.
+#["wow", "I", "am", "really", "learning", "arrays!"]
+binding.pry
 
 end
 
